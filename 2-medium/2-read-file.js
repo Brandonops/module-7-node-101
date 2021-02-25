ab555aec950bc98f14f9820a8bea7cd3cc6f2dc2
Brandon <woolstonbj11@gmail.com>
2021-02-25 12:12:09 -0500

    adding more of the learnyounode exercises

---
+++ 2-medium/2-read-file.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+const fs = require("fs");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question(" Enter a filename: ", (answer) => {
+    console.log(answer)
+
+    fs.readFile(answer, {encoding: "utf-8"}, (error, data) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(data.toUpperCase())
+        }
+    });
+    rl.close();
+});
